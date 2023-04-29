04a9958ddfedf1b77f26ade5499e5d3b38bd4729
Risimon <risimon@mail.ru>
2023-04-30 03:37:39 +0900

    changed API Key

---
+++ Chat.py
@@ -0,0 +1,4 @@
+class Chat:
+    def __init__(self):
+        self.id = None
+        self.messages = []
--- ChatClient.py
+++ ChatClient.py
@@ -1,18 +1,21 @@
 import os
 import openai
+
+import const
 from const import CHAT_PROMPT
 
 from BaseClient import BaseClient
 
 
 class ChatClient(BaseClient):
-    def __init__(self, mode):
+    def __init__(self):
         self.model = "gpt-3.5-turbo"
-        self.mode = mode
+        openai.api_key = const.OPENAI_API_KEY
 
     def respond(self, message_list):
-        prompt = CHAT_PROMPT % message_list[0]
-        messages = [{"role": "system", "content": prompt}, message_list[1:]]
+        prompt = CHAT_PROMPT % message_list[0]["content"]
+        messages = [{"role": "system", "content": prompt}]
+        messages.extend(message_list[1:])
         completion = openai.ChatCompletion.create(
             model=self.model,
             messages=messages
