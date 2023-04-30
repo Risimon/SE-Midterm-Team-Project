dab378d551e1de2443c1b96a37c88dc142117bb4
bonesgone <ulantemirlan2002@gmail.com>
2023-04-30 18:19:31 +0900

    DalleClient

--- DalleClient.py
+++ DalleClient.py
@@ -7,13 +7,11 @@ from BaseClient import BaseClient
 class DalleClient(BaseClient):
     def __init__(self):
         openai.api_key = const.OPENAI_API_KEY
-        self.user = input()
-    
-    def image_generate(self):
+
+    def respond(self, messages):
         response = openai.Image.create(
-            prompt = self.user,
+            prompt = messages,
             n = 1,
             size = "256x256"
         )
-
         return response["data"][0]["url"]
