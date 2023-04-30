16ec0df5d5fe713a1bfe971afdfd0b3ebbcd4a92
Risimon <risimon@mail.ru>
2023-04-30 13:27:21 +0000

    switched to use constants instead of strings

--- const.py
+++ const.py
@@ -10,3 +10,12 @@ CHAT_PROMPT = "As an advanced chatbot Student Assistant, your primary goal is to
               "responses. Use examples and evidence to support your points and justify your recommendations or " \
               "solutions. Remember to always prioritize the needs and satisfaction of the user. Your ultimate goal is " \
               "to provide a helpful and enjoyable experience for the user. "
+
+BUTTON_HELPER = "Student Helper"
+BUTTON_ARTIST = "Artist"
+BUTTON_CLEAR = "Clear"
+
+START_MESSAGE = "Hello %s!\nI am Student Helper Bot\n\nI could be:\n1. Student Assistant\n2. Artist"
+HELPER_MESSAGE = "now i'm a student helper\nplease enter the list of courses you've taken:\n"
+ARTIST_MESSAGE = "now i'm an artist"
+CLEAR_MESSAGE = "Chat context cleared"
