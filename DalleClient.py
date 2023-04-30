f6978fe020529496cfcce6fe3b76d216cfa88e9a
Guliza <58912213+gulizaaitkulova@users.noreply.github.com>
2023-04-30 22:15:07 +0900

    added explanation to the code

--- DalleClient.py
+++ DalleClient.py
@@ -5,9 +5,12 @@ import const
 from BaseClient import BaseClient
 
 class DalleClient(BaseClient):
+    """Class for working with Dalle API. Inherits from BaseClient"""
     def __init__(self, api_key):
+        #set the API KEY
         openai.api_key = api_key
-
+    
+    # generate the image and return the URL
     def respond(self, messages):
         response = openai.Image.create(
             prompt = messages,
